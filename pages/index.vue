27c95c148a5935c9daabe575ae3df415694f61e6
Nikita <nik.veras@mail.ru>
2022-09-25 10:14:31 +0300

    style: minor upgrade

--- pages/index.vue
+++ pages/index.vue
@@ -18,14 +18,16 @@
       реклама
     </div>
     <BaseTitle text="Купить сегодня" />
-    <ProductCard image="assets/images/eda/2.webp" />
     <ProductCard
-      image="assets/images/eda/1.webp"
-      shop-img="assets/images/shops/5.webp"
+      image="https://leonardo.edadeal.io/dyn/cr/catalyst/offers/xsaudroqej6xlh7hbuctdcenlq.jpg"
     />
     <ProductCard
-      image="assets/images/eda/3.webp"
-      shop-img="assets/images/shops/ab.png"
+      image="https://leonardo.edadeal.io/dyn/cr/catalyst/offers/7562vl5ejr66qo3buyi7aljce4.jpg"
+      shop-img="https://leonardo.edadeal.io/dyn/re/retailers/images/icons/sq/06c522eb34f54fed99665f69cb0794b7.png"
+    />
+    <ProductCard
+      image="https://leonardo.edadeal.io/dyn/cr/catalyst/offers/sw4ao6lcebmqfoz3cutqxg7u7e.jpg"
+      shop-img="https://leonardo.edadeal.io/dyn/re/retailers/images/icons/sq/a20f46b5662b4825a29d52b897624916.png"
     />
   </main>
 </template>
